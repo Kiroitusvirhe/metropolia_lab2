20af816c892e77429e94f681b5069b4b037817d2
Santeri Hytönen <santeri.hytonen2@metropolia.fi>
2025-04-03 12:44:16 +0300

    Initial commit: Add Lab2_E1.cpp and multiply.h

---
+++ multiply.h
@@ -0,0 +1,10 @@
+// filepath: c:\Users\Omistaja\Documents\Koulu\C++\VScode Folder\Multiply.h
+#ifndef MULTIPLY_H
+#define MULTIPLY_H
+
+// Function declarations
+int multiply(int a, int b);
+double multiply(double a, double b);
+double multiply(int a, double b, bool flag);
+
+#endif
